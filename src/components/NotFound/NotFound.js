f71f1e705172011a3cd53d13a5eadfcc6a97c0a6
tutulportal <tutulvpn3@gmail.com>
2022-10-11 12:58:37 +0600

    404 not found page added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { MagnifyingGlassMinusIcon } from '@heroicons/react/24/solid';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div className='not-found'>
-            <h1><MagnifyingGlassMinusIcon/></h1>
+            <h2><MagnifyingGlassMinusIcon  className="not-found-icon"/></h2>
             <h1>ROUTE NOT FOUND</h1>
+            <Link to='/home'>Go To Home</Link>
         </div>
     );
 };
