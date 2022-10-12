635fe19588edcf20f37aa665fa2ba91bc1692292
tutulportal <tutulvpn3@gmail.com>
2022-10-12 15:21:11 +0600

    added tostify

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Markup } from 'interweave';
+import { EyeIcon } from '@heroicons/react/24/solid';
 
 const Question = (props) => {
     console.log(props);
-    const {id, question, options} = props.question;
+    const {id, question, options, correctAnswer} = props.question;
+    const checkOption = props.checkOption;
+    const correctAns = props.correctAns;
     return (
         <div className="theQuiz">
             <h2>Question: <Markup content={question} /></h2>
+            <EyeIcon className='eye-icon' onClick={() => correctAns(correctAnswer)}/>
             <div className="quiz-options">
                 {
-                    options.map(option => <li><input type="radio" name={id} id={option} /> <label htmlFor={option}>{option}</label></li>)
+                    options.map(option => <li key={option}><input type="radio" name={id} id={option} onClick={() => checkOption([option, correctAnswer])} /> <label htmlFor={option}>{option}</label></li>)
                 }
             </div>
         </div>
--- src/components/QuizList/QuizList.js
+++ src/components/QuizList/QuizList.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import Question from '../Question/Question';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizList = ({quiz}) => {
     const {name, total, questions} = quiz;
-    console.log(quiz)
+    const correctAns = (data) => {
+        if(data){
+            toast.info(`The Correct Answer is: ${data}`, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        }
+    }
+    const checkOption = (data) => {
+        if(data[0] === data[1]){
+            toast.success('Awesome! The answer is Correct!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        }else{
+            toast.error('Incorrect Answer!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        }
+    }
     return (
         <div className="inner-quiz-wrapper">
             <div className="quiz-header">
@@ -12,8 +52,20 @@ const QuizList = ({quiz}) => {
             </div>
             <div className='quizes'>
                 {
-                    questions.map(question => <Question key={question.id} question={question}></Question>)
+                    questions.map(question => <Question key={question.id} question={question} checkOption={checkOption} correctAns={correctAns}></Question>)
                 }
+                <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                />
             </div>
         </div>
     );
